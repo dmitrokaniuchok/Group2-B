2b8d6465115807491c1c9f7c95470d38415da5b6
DmytroKaniuchok <kanyuchekdmitry@gmail.com>
2025-08-26 17:34:11 +0300

    working

--- src/db/models/Categories.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const categoriesSchema = new mongoose.Schema({
-  name: { type: String, required: true }
-});
-
-export const Categories = mongoose.model("Categories",categoriesSchema);
